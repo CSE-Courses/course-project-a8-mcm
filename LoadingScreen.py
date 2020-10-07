29af74d3a02dfc605619551824bf4a9cecbc3919
Musaiyab-Ali <musaiyab@buffalo.edu>
2020-10-07 17:54:06 -0400

    Created main.py which will contain the refactored main code, also pulled from loadingscreen so those files are here too. The main code is still a work in progress.

---
+++ LoadingScreen.py
@@ -0,0 +1,174 @@
+import pygame
+from pygame.locals import *
+from tqdm import *
+from Search import *
+
+#method for running the loading screen/transition screen
+def loadScreen(currentScreen):
+    #declaring screen, and images used
+    #pygame.init()
+    screen = currentScreen
+    bgIMG = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading1.png")
+    bgIMG2 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading2.png")
+    bgIMG3 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading3.png")
+    bgIMG4 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading4.png")
+    bgIMG5 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading5.png")
+    bgIMG6 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading6.png")
+    bgIMG7 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading7.png")
+    bgIMG8 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading8.png")
+    bgIMG9 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading9.png")
+    bgIMG10 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading10.png")
+    bgIMG11 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading11.png")
+    bgIMG12 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading12.png")
+    bgIMG13 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading13.png")
+    bgIMG14 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading14.png")
+    bgIMG15 = pygame.image.load("../course-project-a8-mcm/images/loadingScreen/loading15.png")
+
+    #loop for loading screen and animation, runs for ~10 seconds
+    loop = tqdm(total = 100, position = 0, leave = False)
+    for k in range(100):
+        loop.set_description("Loading...".format(k))
+        #screen.fill(0)
+        screen.blit(bgIMG, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG2, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG3, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG4, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG5, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG6, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG7, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG8, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG9, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG10, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG11, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG12, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG13, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG14, (1024,464))
+        pygame.display.update()
+        screen.blit(bgIMG15, (1024,464))
+        pygame.display.update()        
+        loop.update(1)
+    loop.close()
+
+class MainMenu:
+    #default sizes for screen resolution
+    screenWid=1280
+    screenLen=720 
+
+    #checker variables to keep track of states
+    insearchbar=0
+    hamState=0
+
+    #the text the user is inputing into the search bar
+    searchbarText=""
+
+    # Empty time string
+    updatedTimeText = ""
+ 
+    def menuInit(self):
+        
+        pygame.init()
+
+        #name of the screen
+        pygame.display.set_caption('MCM')
+        
+        # width then Height
+        screen=pygame.display.set_mode((self.screenWid , self.screenLen))
+        
+        #background image
+        fillerImag=pygame.image.load("../course-project-a8-mcm/images/homepageFiles/Background base.png")
+
+        #menu button
+        hamburgermenu=pygame.image.load("../course-project-a8-mcm/images/homepageFiles/HamburgerMenu.png")
+        
+        #search bar box
+        searchbar=pygame.image.load("../course-project-a8-mcm/images/homepageFiles/SearchBar.png")
+        
+        #button and font for search bar
+        searchBarButton, searchBarFont, updatedTime, timeFont = searchBarInitalize()
+
+        #favorites menu
+        favMenu=pygame.image.load("../course-project-a8-mcm/images/homepageFiles/favorites_background.png")
+
+        loadScreen(screen)
+
+        #screen while program is running
+        while True:
+            #clears the screen
+            screen.fill(0) 
+
+
+            #renders all of the hidden buttons
+            pygame.draw.rect(screen,[0,0,0],searchBarButton)
+
+            #renders background and menu buttons
+            screen.blit(fillerImag, (0,0))
+            screen.blit(searchbar,(0,0))
+            screen.blit(hamburgermenu,(0,0))
+
+            #render search bar text
+            searchtext=searchBarFont.render(self.searchbarText,True,[0,0,0])
+            screen.blit(searchtext,(200,15))
+            
+            if self.hamState==1:
+                screen.blit(favMenu, (0,0))
+                
+            #render hidden buttons
+            hamHidden=pygame.Rect(0,0,100,100)
+
+            # render updated time text
+            timeText = timeFont.render(self.updatedTimeText, True, [0, 0, 0])
+            screen.blit(timeText, (875, 670))
+            # )
+
+
+            #updates the screen
+            pygame.display.update()
+
+            #event handler loop
+            for event in pygame.event.get():
+                #clicking X on window
+                if event.type==pygame.QUIT:
+                    print("Exiting Window")
+                    pygame.quit() 
+                    exit(0)
+
+                #if user clicked hamburger icon
+                if event.type==pygame.MOUSEBUTTONDOWN:
+                    if hamHidden.collidepoint(event.pos):
+                        if self.hamState==0:
+                            self.hamState=1
+                        else:
+                            self.hamState=0
+ 
+                #whether user clicked into search bar
+                if event.type==pygame.MOUSEBUTTONDOWN:
+                    if searchBarButton.collidepoint(event.pos):
+                        self.insearchbar=1
+                    else:
+                        self.insearchbar=0  
+                
+                #if user typed into search bar
+                if event.type==pygame.KEYDOWN and self.insearchbar==1:
+                    if event.unicode=="\r":
+                        if search(self.searchbarText):
+                            self.searchbarText=""
+                            self.updatedTimeText = timeStamp()
+                    else:
+                        self.searchbarText = updateSearchBarOnKeyPress(event, self.searchbarText)
+
+#calls the method to run the program
+test=MainMenu()
+test.menuInit()
