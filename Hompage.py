c8ba80b0c301b32d2c1815fc65e90d5a86e5ee7c
David <davidtjforres@gmail.com>
2020-10-07 18:43:00 -0400

    #31 completed basic representaion of stock icons for homepage

--- Hompage.py
+++ Hompage.py
@@ -17,7 +17,15 @@ def make_buttons_for_stocks():
     stock5=pygame.Rect(20, 550, 1240, 90)
     return stock1, stock2, stock3, stock4, stock5
 
-def make_butt
+def make_buttons_for_page_switching():
+    goback=pygame.Rect(20, 670, 30, 20)
+    numb1=pygame.Rect(60, 670, 30, 20)
+    numb2=pygame.Rect(100, 670, 30, 20)
+    numb3=pygame.Rect(140, 670, 30, 20)
+    numb4=pygame.Rect(180, 670, 30, 20)
+    numb5=pygame.Rect(220, 670, 30, 20)
+    goforward=pygame.Rect(260, 670, 30, 20)
+    return goback, numb1, numb2, numb3, numb4, numb5, goforward
 
 class MainMenu:
     #default sizes for screen resolution
@@ -33,6 +41,8 @@ class MainMenu:
 
     # Empty time string
     updatedTimeText = ""
+    currentPage=1
+    didPageChange=True
 
     stockList=["GILD","WMT","UNP", "UTX","HPQ", "V", "CSCO", "SLB", "AMGN", "BA", "TGT", "COP", "CMCSA", "BMY", "CVX", "VZ", "BP", "T", "UNH", "MCD", "PFE", "ABT", "FB", "DIS", "MMM", "XOM", "ORCL", "PEP","HD", "JPM", "INTC", "WFC", "MRK", "KO", "AMZN", "PG", "BRKB","GOOGL", "GM", "JNJ", "MO", "IBM", "GE", "MSFT", "AAPL","NVDA", "AMD", "GE", "NTDOF", "SNE"]
 
@@ -63,6 +73,10 @@ class MainMenu:
         favMenu=pygame.image.load("../course-project-a8-mcm/images/homepageFiles/favorites_background.png")
 
         stock1, stock2, stock3, stock4, stock5= make_buttons_for_stocks()
+        goback, numb1, numb2, numb3, numb4, numb5, goforward=make_buttons_for_page_switching()
+
+        pagenumber=pygame.font.Font("../course-project-a8-mcm/Fonts/times.ttf",25)
+        stockfont=pygame.font.Font("../course-project-a8-mcm/Fonts/times.ttf",50)
 
         #screen while program is running
         while True:
@@ -98,9 +112,48 @@ class MainMenu:
             screen.blit(timeText, (875, 670))
             # )
 
+            pygame.draw.rect(screen,[255,0,0],numb1)
+            pygame.draw.rect(screen,[255,0,0],numb2)
+            pygame.draw.rect(screen,[255,0,0],numb3)
+            pygame.draw.rect(screen,[255,0,0],numb4)
+            pygame.draw.rect(screen,[255,0,0],numb5)
+            pygame.draw.rect(screen,[0,255,0],goback)
+            pygame.draw.rect(screen,[0,255,0],goforward)
 
 
 
+            firstStock=stockfont.render(self.stockList[self.currentPage*5-5], True,[255,0,0])
+            secondStock=stockfont.render(self.stockList[self.currentPage*5-4], True,[255,0,0])
+            thirdStock=stockfont.render(self.stockList[self.currentPage*5-3], True,[255,0,0])
+            fourthStock=stockfont.render(self.stockList[self.currentPage*5-2], True,[255,0,0])
+            fifthStock=stockfont.render(self.stockList[self.currentPage*5-1], True,[255,0,0])
+            screen.blit(firstStock,(30, 110))
+            screen.blit(secondStock,(30, 220))
+            screen.blit(thirdStock,(30, 330))
+            screen.blit(fourthStock,(30, 440))
+            screen.blit(fifthStock,(30, 550))
+            if self.currentPage<6:
+                wan=pagenumber.render(str(self.currentPage),True,[0,0,0])
+                two=pagenumber.render(str(self.currentPage+1),True,[0,0,0])
+                three=pagenumber.render(str(self.currentPage+2),True,[0,0,0])
+                four=pagenumber.render(str(self.currentPage+3),True,[0,0,0])
+                five=pagenumber.render(str(self.currentPage+4),True,[0,0,0])
+                screen.blit(wan,(68,665))
+                screen.blit(two,(108,665))
+                screen.blit(three,(148,665))
+                screen.blit(four,(188,665))
+                screen.blit(five,(228,665))
+            else:
+                wan=pagenumber.render("6",True,[0,0,0])
+                two=pagenumber.render("7",True,[0,0,0])
+                three=pagenumber.render("8",True,[0,0,0])
+                four=pagenumber.render("9",True,[0,0,0])
+                five=pagenumber.render("10",True,[0,0,0])
+                screen.blit(wan,(68,665))
+                screen.blit(two,(108,665))
+                screen.blit(three,(148,665))
+                screen.blit(four,(188,665))
+                screen.blit(five,(222,665))
             #updates the screen
             pygame.display.update()
 
@@ -119,6 +172,51 @@ class MainMenu:
                             self.hamState=1
                         else:
                             self.hamState=0
+                    pos= event.pos
+                    if goback.collidepoint(pos) and self.currentPage > 1:
+                        self.currentPage-=1
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    if goforward.collidepoint(pos) and self.currentPage<10:
+                        self.currentPage+=1
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    if numb1.collidepoint(pos) and self.currentPage>5:
+                        self.currentPage=6
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    if numb2.collidepoint(pos) and self.currentPage<5:
+                        self.currentPage+=1
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    elif numb2.collidepoint(pos):
+                        self.currentPage=7
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    if numb3.collidepoint(pos) and self.currentPage<5:
+                        self.currentPage+=2
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    elif numb3.collidepoint(pos):
+                        self.currentPage=8
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    if numb4.collidepoint(pos) and self.currentPage<5:
+                        self.currentPage+=3
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    elif numb4.collidepoint(pos):
+                        self.currentPage=9
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    if numb5.collidepoint(pos) and self.currentPage<5:
+                        self.currentPage+=4
+                        self.didPageChange=True
+                        print(str(self.currentPage))
+                    elif numb5.collidepoint(pos):
+                        self.currentPage=10
+                        self.didPageChange=True
+                        print(str(self.currentPage))
  
                 #whether user clicked into search bar
                 if event.type==pygame.MOUSEBUTTONDOWN:
