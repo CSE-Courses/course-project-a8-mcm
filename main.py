f359d4f5745aa3d79b758cdcd5fd3c135afb2c2b
David <davidtjforres@gmail.com>
2020-10-28 18:06:42 -0400

    task #60 completed, main.py is refactored to homepage's standards

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from Search import *
 from LoadingScreen import *
+from TempInfoPage import*
 
 """
 @Authors: Musaiyab Ali, David Forrest
@@ -30,6 +31,15 @@ class MainRunner:
     fav2 = ""
     fav3 = ""
 
+    # The current tab the user is on
+    currentPage=1
+    didPageChange=True
+
+    #the list of stocks
+    stockList=["GILD","WMT","UNP", "GPRO","HPQ", "V", "CSCO", "SLB", "AMGN", "BA", "TGT", "COP", "CMCSA", "BMY", "CVX", "VZ", "BP", "T", "UNH", "MCD", "PFE", "ABT", "FB", "DIS", "MMM", "XOM", "ORCL", "PEP","HD", "JPM", "INTC", "WFC", "MRK", "KO", "AMZN", "PG", "BRKB","GOOGL", "GM", "JNJ", "MO", "IBM", "GE", "MSFT", "AAPL","NVDA", "AMD", "GE", "NTDOF", "SNE"]
+
+
+
     #function for initializing images
     def imgInit(self):
         #loading images
@@ -43,13 +53,32 @@ class MainRunner:
         favMenu=pygame.image.load("../course-project-a8-mcm/images/homepageFiles/favorites_background.png")
         #search bar image
         searchbar=pygame.image.load("../course-project-a8-mcm/images/homepageFiles/SearchBar.png")
+        #homepage tab images
+        hompeageIcons=pygame.image.load("../course-project-a8-mcm/images/homepageFiles/hompage Icons.png")
 
-        return loadImg1, loadImg2, fillerImag, hamburgermenu, favMenu, searchbar
+        return loadImg1, loadImg2, fillerImag, hamburgermenu, favMenu, searchbar, hompeageIcons
 
     def buttonInit(self):
         #render hidden buttons
         hamHidden=pygame.Rect(0,0,100,100)
-        return hamHidden
+
+        #render buttons for stocks
+        stock1=pygame.Rect(20, 110, 1240, 90)
+        stock2=pygame.Rect(20, 220, 1240, 90)
+        stock3=pygame.Rect(20, 330, 1240, 90)
+        stock4=pygame.Rect(20, 440, 1240, 90)
+        stock5=pygame.Rect(20, 550, 1240, 90)
+
+        #render buttons for switching between pages
+        goback=pygame.Rect(20, 670, 30, 20)
+        numb1=pygame.Rect(60, 670, 30, 20)
+        numb2=pygame.Rect(100, 670, 30, 20)
+        numb3=pygame.Rect(140, 670, 30, 20)
+        numb4=pygame.Rect(180, 670, 30, 20)
+        numb5=pygame.Rect(220, 670, 30, 20)
+        goforward=pygame.Rect(260, 670, 30, 20)
+
+        return hamHidden, stock1, stock2, stock3, stock4, stock5, goback, numb1, numb2, numb3, numb4, numb5, goforward
 
     def readFile(self):
         theFile= open("settings.txt")
@@ -67,6 +96,11 @@ class MainRunner:
             
         theFile.close()
 
+    def fontInit(self):
+        pagenumber=pygame.font.Font("../course-project-a8-mcm/Fonts/times.ttf",25)
+        stockfont=pygame.font.Font("../course-project-a8-mcm/Fonts/times.ttf",65)
+        return pagenumber, stockfont
+
     def mainScreen(self):
         
         pygame.init()
@@ -78,11 +112,16 @@ class MainRunner:
         screen=pygame.display.set_mode((self.screenWid , self.screenLen))    
         
         #calls method to initialize all the images
-        loadImg1, loadImg2, fillerImag, hamburgermenu, favMenu, searchbar = self.imgInit()
+        loadImg1, loadImg2, fillerImag, hamburgermenu, favMenu, searchbar, homepageIcons = self.imgInit()
 
         #calls method from Search to make button and font for search bar
-        searchBarButton, searchBarFont, updatedTime, timeFont, companyFont, favFont = searchBarInitalize()       
+        searchBarButton, searchBarFont, updatedTime, timeFont, companyFont, favFont = searchBarInitalize() 
 
+        #calls method to initalize make the buttons
+        hamHidden, stock1, stock2, stock3, stock4, stock5, goback, numb1, numb2, numb3, numb4, numb5, goforward=self.buttonInit()
+       
+       #calls method to initalize fonts
+        pagenumber, stockfont=self.fontInit()
         loadScreen(screen) 
 
         #screen while program is running
@@ -93,8 +132,6 @@ class MainRunner:
             #renders all of the hidden buttons
             pygame.draw.rect(screen,[0,0,0],searchBarButton)
 
-            #calls method to initialize buttons
-            hamHidden = self.buttonInit()
 
             #renders background and menu buttons
             screen.blit(fillerImag, (0,0))
@@ -104,6 +141,109 @@ class MainRunner:
             #render search bar text
             searchtext=searchBarFont.render(self.searchbarText,True,[0,0,0])
             screen.blit(searchtext,(200,15))
+
+            #renders hompage hiddenButtons
+            pygame.draw.rect(screen,[0,0,0],stock1)
+            pygame.draw.rect(screen,[0,0,0],stock2)
+            pygame.draw.rect(screen,[0,0,0],stock3)
+            pygame.draw.rect(screen,[0,0,0],stock4)
+            pygame.draw.rect(screen,[0,0,0],stock5)
+
+            #renders hompage page switching buttons
+            pygame.draw.rect(screen,[255,0,0],numb1)
+            pygame.draw.rect(screen,[255,0,0],numb2)
+            pygame.draw.rect(screen,[255,0,0],numb3)
+            pygame.draw.rect(screen,[255,0,0],numb4)
+            pygame.draw.rect(screen,[255,0,0],numb5)
+            pygame.draw.rect(screen,[0,255,0],goback)
+            pygame.draw.rect(screen,[0,255,0],goforward)
+            screen.blit(homepageIcons,(0,0))
+
+            #renders stock text for each stock on display
+            firstStock=stockfont.render(self.stockList[self.currentPage*5-5], True,[0,0,0])
+            secondStock=stockfont.render(self.stockList[self.currentPage*5-4], True,[0,0,0])
+            thirdStock=stockfont.render(self.stockList[self.currentPage*5-3], True,[0,0,0])
+            fourthStock=stockfont.render(self.stockList[self.currentPage*5-2], True,[0,0,0])
+            fifthStock=stockfont.render(self.stockList[self.currentPage*5-1], True,[0,0,0])
+            screen.blit(firstStock,(30, 110))
+            screen.blit(secondStock,(30, 220))
+            screen.blit(thirdStock,(30, 330))
+            screen.blit(fourthStock,(30, 440))
+            screen.blit(fifthStock,(30, 550))
+
+            #handles page switching
+            if self.currentPage<6:
+                if self.currentPage==1:
+                    one=pagenumber.render("1",True,[255,255,255])
+                    two=pagenumber.render("2",True,[0,0,0])
+                    three=pagenumber.render("3",True,[0,0,0])
+                    four=pagenumber.render("4",True,[0,0,0])
+                    five=pagenumber.render("5",True,[0,0,0])
+                elif self.currentPage==2:
+                    one=pagenumber.render("1",True,[0,0,0])
+                    two=pagenumber.render("2",True,[255,255,255])
+                    three=pagenumber.render("3",True,[0,0,0])
+                    four=pagenumber.render("4",True,[0,0,0])
+                    five=pagenumber.render("5",True,[0,0,0])
+                elif self.currentPage==3:
+                    one=pagenumber.render("1",True,[0,0,0])
+                    two=pagenumber.render("2",True,[0,0,0])
+                    three=pagenumber.render("3",True,[255,255,255])
+                    four=pagenumber.render("4",True,[0,0,0])
+                    five=pagenumber.render("5",True,[0,0,0])
+                elif self.currentPage==4:
+                    one=pagenumber.render("1",True,[0,0,0])
+                    two=pagenumber.render("2",True,[0,0,0])
+                    three=pagenumber.render("3",True,[0,0,0])
+                    four=pagenumber.render("4",True,[255,255,255])
+                    five=pagenumber.render("5",True,[0,0,0])
+                else:
+                    one=pagenumber.render("1",True,[0,0,0])
+                    two=pagenumber.render("2",True,[0,0,0])
+                    three=pagenumber.render("3",True,[0,0,0])
+                    four=pagenumber.render("4",True,[0,0,0])
+                    five=pagenumber.render("5",True,[255,255,255])
+                screen.blit(one,(68,665))
+                screen.blit(two,(108,665))
+                screen.blit(three,(148,665))
+                screen.blit(four,(188,665))
+                screen.blit(five,(228,665))
+            else:
+                if self.currentPage==6:
+                    one=pagenumber.render("6",True,[255,255,255])
+                    two=pagenumber.render("7",True,[0,0,0])
+                    three=pagenumber.render("8",True,[0,0,0])
+                    four=pagenumber.render("9",True,[0,0,0])
+                    five=pagenumber.render("10",True,[0,0,0])
+                elif self.currentPage==7:
+                    one=pagenumber.render("6",True,[0,0,0])
+                    two=pagenumber.render("7",True,[255,255,255])
+                    three=pagenumber.render("8",True,[0,0,0])
+                    four=pagenumber.render("9",True,[0,0,0])
+                    five=pagenumber.render("10",True,[0,0,0])
+                elif self.currentPage==8:
+                    one=pagenumber.render("6",True,[0,0,0])
+                    two=pagenumber.render("7",True,[0,0,0])
+                    three=pagenumber.render("8",True,[255,255,255])
+                    four=pagenumber.render("9",True,[0,0,0])
+                    five=pagenumber.render("10",True,[0,0,0])
+                elif self.currentPage==9:
+                    one=pagenumber.render("6",True,[0,0,0])
+                    two=pagenumber.render("7",True,[0,0,0])
+                    three=pagenumber.render("8",True,[0,0,0])
+                    four=pagenumber.render("9",True,[255,255,255])
+                    five=pagenumber.render("10",True,[0,0,0])
+                else:
+                    one=pagenumber.render("6",True,[0,0,0])
+                    two=pagenumber.render("7",True,[0,0,0])
+                    three=pagenumber.render("8",True,[0,0,0])
+                    four=pagenumber.render("9",True,[0,0,0])
+                    five=pagenumber.render("10",True,[255,255,255])
+                screen.blit(one,(68,665))
+                screen.blit(two,(108,665))
+                screen.blit(three,(148,665))
+                screen.blit(four,(188,665))
+                screen.blit(five,(222,665))
                             
             # render updated time text
             timeText = timeFont.render(self.updatedTimeText, True, [0, 0, 0])
@@ -134,6 +274,81 @@ class MainRunner:
 
                #if user clicked left mouse button
                 if event.type==pygame.MOUSEBUTTONDOWN:
+                    pos= event.pos
+                    #handle opening the info page when clicking on the stock
+                    if stock1.collidepoint(event.pos):
+                        newPage=InfoPage()
+                        newPage.setStock(self.stockList[self.currentPage*5-5])
+                        newPage.infoPgInit()
+                    if stock2.collidepoint(event.pos):
+                        newPage=InfoPage()
+                        newPage.setStock(self.stockList[self.currentPage*5-4])
+                        newPage.infoPgInit()
+                    if stock3.collidepoint(event.pos):
+                        newPage=InfoPage()
+                        newPage.setStock(self.stockList[self.currentPage*5-3])
+                        newPage.infoPgInit()
+                    if stock4.collidepoint(event.pos):
+                        newPage=InfoPage()
+                        newPage.setStock(self.stockList[self.currentPage*5-2])
+                        newPage.infoPgInit()
+                    if stock5.collidepoint(event.pos):
+                        newPage=InfoPage()
+                        newPage.setStock(self.stockList[self.currentPage*5-1])
+                        newPage.infoPgInit()
+
+
+                        #handles tabing between pages
+
+                    if goback.collidepoint(pos) and self.currentPage > 1:
+                        self.currentPage-=1
+                        self.didPageChange=True
+                        #print(str(self.currentPage))
+                    if goforward.collidepoint(pos) and self.currentPage<10:
+                        self.currentPage+=1
+                        self.didPageChange=True
+                       # print(str(self.currentPage))
+                    if numb1.collidepoint(pos) and self.currentPage>5:
+                        self.currentPage=6
+                        self.didPageChange=True
+                       # print(str(self.currentPage))
+                    elif numb1.collidepoint(pos):
+                        self.currentPage=1
+                        self.didPageChange=True
+                      #  print(str(self.currentPage))
+                    if numb2.collidepoint(pos) and self.currentPage<=5:
+                        self.currentPage=2
+                        self.didPageChange=True
+                        #print(str(self.currentPage))
+                    elif numb2.collidepoint(pos):
+                        self.currentPage=7
+                        self.didPageChange=True
+                        #print(str(self.currentPage))
+                    if numb3.collidepoint(pos) and self.currentPage<=5:
+                        self.currentPage=3
+                        self.didPageChange=True
+                        #print(str(self.currentPage))
+                    elif numb3.collidepoint(pos):
+                        self.currentPage=8
+                        self.didPageChange=True
+                      #  print(str(self.currentPage))
+                    if numb4.collidepoint(pos) and self.currentPage<=5:
+                        self.currentPage=4
+                        self.didPageChange=True
+                       # print(str(self.currentPage))
+                    elif numb4.collidepoint(pos):
+                        self.currentPage=9
+                        self.didPageChange=True
+                       # print(str(self.currentPage))
+                    if numb5.collidepoint(pos) and self.currentPage<=5:
+                        self.currentPage=5
+                        self.didPageChange=True
+                        #print(str(self.currentPage))
+                    elif numb5.collidepoint(pos):
+                        self.currentPage=10
+                        self.didPageChange=True
+                      #  print(str(self.currentPage))
+
                     if hamHidden.collidepoint(event.pos):
                         if self.hamState==0:
                             self.hamState=1
@@ -148,6 +363,9 @@ class MainRunner:
                 if event.type==pygame.KEYDOWN and self.insearchbar==1:
                     if event.unicode=="\r":
                         if search(self.searchbarText):
+                            newPage=InfoPage()
+                            newPage.setStock(self.searchbarText)
+                            newPage.infoPgInit()
                             self.searchbarText=""
                             self.updatedTimeText = timeStamp()
                     else:
