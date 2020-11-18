e1f378168b4a7fa428075638dfb64a0ebab6d733
Musaiyab-Ali <musaiyab@buffalo.edu>
2020-11-18 17:06:06 -0500

    Added delFav, modified writeFile, and edited tests

--- UnitTestFolder/WriteTest.py
+++ UnitTestFolder/WriteTest.py
@@ -3,22 +3,44 @@ class writeTest:
     fav2="WMT"
     fav3="CSCO"
 
-    #unit test for writeFile
-    def writeFile(self, str1, str2):
+    def writeFile(self, str):
+        if self.fav1 != "" and self.fav2 != "" and self.fav3 != "":
+            print("Favorite list is full!")
+        elif self.fav1 == "":
+            self.fav1 = str
+        elif self.fav2 == "":
+            self.fav2 = str
+        elif self.fav3 == "":
+            self.fav3 = str
 
-        if str1 == "fav1":
-            self.fav1 = str2
-        if str1 == "fav2":
-            self.fav2 = str2
-        if str1 == "fav3":
-            self.fav3 = str2
-
-        theFile= open("../settings.txt", "w")
+        theFile= open("testSettings.txt", "w")
         theFile.write("fav1=" + self.fav1 + "=\n")
         theFile.write("fav2=" + self.fav2 + "=\n")
         theFile.write("fav3=" + self.fav3 + "=\n")
 
         theFile.close()
 
+    def delFav(self, str):
+        if str == "fav1":
+            self.fav1 = ""
+        if str == "fav2":
+            self.fav2 = ""            
+        if str == "fav3":
+            self.fav3 = ""    
+            print(self.fav3)
+
+        #print(self.fav1)
+        #print(self.fav2)
+        #print(self.fav3)
+
+        theFile= open("testSettings.txt", "w")
+        theFile.write("fav1=" + self.fav1 + "=\n")
+        theFile.write("fav2=" + self.fav2 + "=\n")
+        theFile.write("fav3=" + self.fav3 + "=\n")
+
+        theFile.close()
+
+#unit test for writeFile
 test = writeTest()
-test.writeFile("fav3", "AMZN")
+test.delFav("fav1")
+#test.writeFile("PPPQ")
